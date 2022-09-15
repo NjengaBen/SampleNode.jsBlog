c185d6fe6df262f8e6ba718b3ab5a98eb913fa66
NjengaBen <bennjenga2@gmail.com>
2022-09-16 01:53:52 +0300

    Changes in stylesheet route

--- app.js
+++ app.js
@@ -50,16 +50,16 @@ app.post('/blogs', (req, res) => {
 })
 
 app.get('/blogs/create', (req, res) =>{
-    res.render('create', {title: 'New Blog'});    
+    res.render('create', {title: 'New Blog'})    
 })
 
 app.get('/blogs/:id', (req, res) =>{
     const id = req.params.id
     Blog.findById(id)
         .then(result =>{
-            res.render('details', {blog: result, title: 'Blog Details'});
+            res.render('details', {blog: result, title: 'Blog Details'})
         })
-        .catch(err => console.log(err));
+        .catch(err => console.log(err))
 })
 
 app.delete('/blogs/:id', (req, res) => {
